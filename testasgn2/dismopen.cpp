018d0b738838bfdb5a08a11e87cee41c89a11c0b
kcarella <kcarella@ucsc.edu>
2019-02-13 16:32:16 -0800

    multithread open()

--- testasgn2/dismopen.cpp
+++ testasgn2/dismopen.cpp
@@ -18,25 +18,40 @@
 #include<pthread.h>
 #include<semaphore.h>
 
+/*This program shows how different threads run at different speeds.
+after multiple runs the outputs will be different. With threads
+completing at different times despite order in which orders are called*/
+
 struct thread_info
 {
 	int id;
-	sem_t* dipsem_addr; 
+	sem_t* dipsem_addr;
+	int fd = -6868; 
 };
 
 
 void* thread_func(void* arg)
 {
 	struct thread_info* ti_ptr = (thread_info*) arg;
-	printf("Hi! from thread %d, I'll wait now..\n", ti_ptr->id);
+	printf("thread %d: Hi! I'll wait now..\n", ti_ptr->id);
 	sem_wait(ti_ptr->dipsem_addr);
+	ti_ptr->fd = open("foo.txt", O_RDONLY);
+	char readbuf[1000];
+	int readval = read(ti_ptr->fd, readbuf, 999);
+	if(readval < 0)
+	{
+		printf("thread %d: read failed\n", ti_ptr->id);
+		return NULL;
+	}
+	printf("thread %d: I read| %s\n", ti_ptr->id, readbuf);
+	sleep(2);
+	printf("thread %d: I have fd %d\n", ti_ptr->id, ti_ptr->fd);
+	printf("thread %d: I'm DONE\n", ti_ptr->id);
 
-	printf("thread %d is finished\n", ti_ptr->id);
 }
 
 void dispatcher(sem_t* addrof_dipsem)
 {
-	//Question: create semaphore here or in main?
 	//CREATE THREADS
 	pthread_t th_array[3];
 	struct thread_info tinfo_array[3];
@@ -46,17 +61,16 @@ void dispatcher(sem_t* addrof_dipsem)
 		tinfo_array[i].dipsem_addr = addrof_dipsem;
 		pthread_create(&th_array[i], NULL, thread_func, &tinfo_array[i]);
 	}
-	for(int i; i < 10000; i++)
-	{
-
-	}
 	//END CREATE THREADS
 	sleep(2);
-	printf("I will awaken one thread now\n");
+	printf("dispatcher:I will awaken one thread now\n");
 	sem_post(addrof_dipsem);
-	sleep(2);
-	printf("I will awaken another thread now\n");
+	printf("dispatcher: I will awaken another thread now\n");
 	sem_post(addrof_dipsem);
+	printf("dispatcher: and the final one\n");
+	sem_post(addrof_dipsem);
+	sleep(3);
+	printf("dispatcher: end of dispatcher\n");
 
 }
 
@@ -66,7 +80,7 @@ int main(int argc, char** argv)
 	sem_t dipsem;
 	sem_init(&dipsem, 0, 0); //initialize sem
 	dispatcher(&dipsem);
-
+	printf("Main: Main will now loop forever\n");
 	for(;;);
 	return 0;
 }
